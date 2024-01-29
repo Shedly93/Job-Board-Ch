3b934035ef00521e98db03931ccbcc2666704ea5
Shedly93 <shedlytrshl.1@gmail.com>
2024-01-29 18:31:48 +0100

    first commit

---
+++ config/config.php
@@ -0,0 +1,12 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$database = "job1";
+
+$conn = new mysqli($servername, $username, $password, $database);
+
+if ($conn->connect_error) {
+    die("La connexion à la base de données a échoué : " . $conn->connect_error);
+}
+?>
